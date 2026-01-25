818d9ae611be7a5e7d83d3ab1c5d766b8b5c355c
in seong Park <74069492+Hexeong@users.noreply.github.com>
2026-01-25 21:35:42 +0900

    [BE] feat: Nginx 설정 및 CI/CD 파이프라인(GitHub Actions) 설정 (#58)
    * feat: nginx.conf 파일 생성
    
    * feat: be-ci.yml 구현
    - ci/cd workflows 스크립트 파일을 로컬 테스트하기 위한 시크릿 파일을 .gitignore에 추가
    - application-local에 datasource.url을 상수값인 localhost로 변경
    - SSH 터널링을 활용한 be-ci.yml 파일 생성
    
    * feat: be-cd.yml 구현
    - Springboot 앱을 도커 이미지로 빌드하기 위한 Dockerfile 생성
    - Remote Web 인스턴스에 배포하기 위한 be-cd.yml 파일 생성
    
    * fix: Nginx 설정에 보안 헤더 추가
    
    * fix: 컨테이너 실행 시 최소 권한 원칙으로 실행되도록 수정
    
    * refactor: Test Report를 PR에서 볼 수 있도록 리팩토링
    - JUnit Test 결과에 대한 report를 PR에 생성
    - Jacoco를 사용해 문장 기반의 Test Coverage 측정 결과를 PR에 생성
    - build.gradle에 jacoco 관련 내용 추가
    
    * refactor: be-ci 진행 시 TestContainer를 사용하도록 수정
    - build.gradle의 springboot 버전을 3.5.10으로 변경(4.0.1은 TestContainer 관련 의존성이 없음)
    - BeApplicationTests를 TestContainer를 사용하도록 수정
    - be-ci.yml에 테스트 컨테이너 반영하여 수정
    
    * fix: BE 폴더와 be-cd.yml 변경시에만 트리거 되도록 수정

--- BE/src/test/java/com/navisa/be/BeApplicationTests.java
+++ BE/src/test/java/com/navisa/be/BeApplicationTests.java
@@ -2,9 +2,18 @@ package com.navisa.be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BeApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:17.7");
 
     @Test
     void contextLoads() {
