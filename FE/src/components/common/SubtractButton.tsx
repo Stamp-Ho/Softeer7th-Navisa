b7fd9f31533201a6a02f4284ddda176473d55717
Stamp-Ho <137289612+Stamp-Ho@users.noreply.github.com>
2026-02-20 13:58:12 +0900

    [FE] fix: 정말 중요한 버튼 하나 수정 (#328)
    * fix: vercel 오류 해결
    
    * fix: form inputline 삭제 버튼 타입 수정

--- FE/src/components/common/SubtractButton.tsx
+++ FE/src/components/common/SubtractButton.tsx
@@ -6,6 +6,7 @@ const SubtractButton = ({ className = "", onClick = () => {} }) => {
       className={`${className} cursor-pointer flex-row flex items-center justify-center rounded-lg bg-gray-150
         w-14 h-14 focus:outline-violet-100 focus:outline-2`}
       onClick={onClick}
+      type="button"
     >
       <IcDash color="var(--gray-500)" />
     </button>
